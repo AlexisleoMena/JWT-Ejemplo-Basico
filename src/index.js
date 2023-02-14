b8aff6412f8fcc99640ffc53acd04fcebe626bf2
AlexisleoMena <alexismena2690@gmail.com>
2023-02-13 21:49:22 -0300

    Establecimos la conexión con la db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const app = require("./app")
+const app = require("./app");
+require("./db.js")
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
